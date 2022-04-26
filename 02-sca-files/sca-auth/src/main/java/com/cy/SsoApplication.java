1686d2a9b43c726a0526cfae01c93af2b6b4aea5
KKOBE12345 <1530800243@qq.com>
2022-04-27 04:11:40 +0800

    dev001分支只是在加了点注释

--- 02-sca-files/sca-auth/src/main/java/com/cy/SsoApplication.java
+++ 02-sca-files/sca-auth/src/main/java/com/cy/SsoApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients   //在dev分支上实现bug修复
+@EnableFeignClients   //在dev001分支上实现bug修复
 public class SsoApplication {
 
     //这是一个统一的认证中心   实现单点登录系统  SSO
 
     public static void main(String[] args) {
         SpringApplication.run(SsoApplication.class, args);
+        System.out.println("123branch go go go");
     }
 }
